a2943abcef613b9e8a278aeb3123645044730c32
karitra <karev.alexandre@gamil.com>
2016-12-19 14:09:34 +0300

    update of native service

--- http1-init.cc
+++ http1-init.cc
@@ -1,35 +1,58 @@
 #include <thread>
+#include <iostream>
 #include <chrono>
+
 #include <cstdlib>
 
 #include <cocaine/framework/worker.hpp>
 #include <cocaine/framework/worker/http.hpp>
+#include <cocaine/framework/trace.hpp>
+#include <cocaine/framework/trace_logger.hpp>
 
+using namespace cocaine;
 using namespace cocaine::framework;
 using namespace cocaine::framework::worker;
+
+namespace {
+  constexpr auto appName = "PingPong";
+}
 
 int main(int argc, char** argv) {
 
     //using namespace std::chrono_literals;
+    using namespace std;
+
+
+    const auto var1 = getenv("TEST1");
 
     worker_t worker(options_t(argc, argv));
 
-    const std::array<size_t, 3> delays = {1,3,7};
+    auto trace = trace_t::generate( string(appName).append("::main") );
+    trace_t::restore_scope_t scope(trace);
 
+    //
+    // Entry web point
+    // TODO:
+    //    - port selection
+    //    - parameters
+    //
     typedef http::event<> http_t;
-    worker.on<http_t>("http", [&delays](http_t::fresh_sender tx, http_t::fresh_receiver){
+    worker.on<http_t>("http", [](http_t::fresh_sender tx, http_t::fresh_receiver){
         http_response_t rs;
         rs.code = 200;
 
-        std::chrono::seconds delta(delays[ std::rand() % delays.size()]);
+        cout << "on web request\n";
 
         // TODO: logging
-        std::this_thread::sleep_for(delta);
+        // std::this_thread::sleep_for(delta);
 
         tx.send(std::move(rs)).get()
             .send("Hello coxxx user from C++").get();
 
+        cout << "web request done\n";
     });
+
+     // worker.on
 
     return worker.run();
 }
